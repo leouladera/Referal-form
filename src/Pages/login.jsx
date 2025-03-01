dda5ed09d6e69b05442d9d48d8cf6a4087d6bed8
leouladera <rover7614@gmail.com>
2025-03-01 12:42:51 +0300

    responsive design

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -32,69 +32,75 @@ function SignUp() {
   };
   return (
     <>
-      <div className={styles.leftTitle}>
-        <h1 className={styles.title}>AIS discipline referral form</h1>
-        <div className={styles.line}></div>
-      </div>
-
       <div className={styles.container}>
-        <div className={styles.Buttons}>
-          <div className={styles.SignUP} onClick={handleSignUpClick}>
-            Signup
-          </div>
-          <div className={styles.lineButton}></div>
-          <div className={styles.LogIN} onClick={handleLoginClick}>
-            Login
+        <div className={styles.containerLeft}>
+          <div className={styles.leftTitle}>
+            <h1 className={styles.title}>
+              AIS discipline <br /> referral form
+            </h1>
+            <div className={styles.line}></div>
           </div>
         </div>
-        <h1 className={styles.welcome}>welcome back!</h1>
-        <p>Email</p>
-        <input
-          className={styles.email}
-          name="email"
-          id=""
-          placeholder="you@example.com"
-          type="email"
-          required
-        ></input>
-        <p className={styles.pPassword}>Password</p>
-        <input
-          className={styles.password}
-          name="email"
-          id=""
-          placeholder="Password"
-          type="password"
-          required
-        ></input>
-        <button
-          type="submit"
-          className={styles.Submit}
-          onClick={() => {
-            click();
-            navigateHomeHandler();
-          }}
-        >
-          {text}
-        </button>
-        <p className={styles.Create}>
-          Don&apos;t have an account yet?
-          <span>
-            <a onClick={handleSignUpClick}> Create one</a>
-          </span>
-        </p>
-        <div className={styles.lineGoogle}>
-          <div className={styles.lines}></div>
-          <p className={styles.signInGoogle}>or Signup with</p>
-          <div className={styles.lines}></div>
+        {/* ................................................................................................... */}
+        <div className={styles.containerRight}>
+          <div className={styles.Buttons}>
+            <div className={styles.SignUP} onClick={handleSignUpClick}>
+              Signup
+            </div>
+            <div className={styles.lineButton}></div>
+            <div className={styles.LogIN} onClick={handleLoginClick}>
+              Login
+            </div>
+          </div>
+          <h1 className={styles.welcome}>welcome back!</h1>
+          <p>Email</p>
+          <input
+            className={styles.email}
+            name="email"
+            id=""
+            placeholder="you@example.com"
+            type="email"
+            required
+          ></input>
+          <p className={styles.pPassword}>Password</p>
+          <input
+            className={styles.password}
+            name="email"
+            id=""
+            placeholder="Password"
+            type="password"
+            required
+          ></input>
+          <button
+            type="submit"
+            className={styles.Submit}
+            onClick={() => {
+              click();
+              navigateHomeHandler();
+            }}
+          >
+            {text}
+          </button>
+          <p className={styles.Create}>
+            Don&apos;t have an account yet?
+            <span>
+              <a onClick={handleSignUpClick}> Create one</a>
+            </span>
+          </p>
+          <div className={styles.lineGoogle}>
+            <div className={styles.lines}></div>
+            <p className={styles.signInGoogle}>or Signup with</p>
+            <div className={styles.lines}></div>
+          </div>
+          <button className={styles.google} onClick={googleClick}>
+            <FontAwesomeIcon
+              icon={faGoogle}
+              fontSize={25}
+              style={{ marginRight: "10px" }}
+            ></FontAwesomeIcon>
+            {GoogleText}
+          </button>
         </div>
-        <button className={styles.google} onClick={googleClick}>
-          <FontAwesomeIcon
-            icon={faGoogle}
-            fontSize={25}
-            style={{ marginRight: "10px" }}
-          ></FontAwesomeIcon>
-          {GoogleText}
-        </button>
       </div>
       <footer>© andinet international school</footer>
     </>
--- src/Pages/signup.jsx
+++ src/Pages/signup.jsx
@@ -33,90 +33,98 @@ function SignUp() {
   };
   return (
     <>
-      <div className={styles.leftTitle}>
-        <h1 className={styles.title}>AIS discipline referral form</h1>
-        <div className={styles.line}></div>
-      </div>
-
       <div className={styles.container}>
-        <div className={styles.Buttons}>
-          <div className={styles.SignUP} onClick={handleSignUpClick}>
-            Signup
-          </div>
-          <div className={styles.lineButton}></div>
-          <div className={styles.LogIN} onClick={handleLoginClick}>
-            Login
+        <div className={styles.containerLeft}>
+          <div className={styles.leftTitle}>
+            <h1 className={styles.title}>
+              AIS discipline <br />
+              referral form
+            </h1>
+            <div className={styles.line}></div>
           </div>
         </div>
-        <h1 className={styles.welcome}>SignUp</h1>
-        <p>Full Name:</p>
-        <div className={styles.nameContainer}>
+        {/* <h1>test</h1> */}
+
+        <div className={styles.containerRight}>
+          <div className={styles.Buttons}>
+            <div className={styles.SignUP} onClick={handleSignUpClick}>
+              Signup
+            </div>
+            <div className={styles.lineButton}></div>
+            <div className={styles.LogIN} onClick={handleLoginClick}>
+              Login
+            </div>
+          </div>
+          <h1 className={styles.welcome}>SignUp</h1>
+          <p>Full Name:</p>
+          <div className={styles.nameContainer}>
+            <input
+              className={styles.name}
+              name="email"
+              id=""
+              placeholder="First Name"
+              type="text"
+              required
+            ></input>
+            <input
+              className={styles.name}
+              name="email"
+              id=""
+              placeholder="Last Name"
+              type="email"
+              required
+            ></input>
+          </div>
+          <p>Email:</p>
           <input
-            className={styles.name}
+            className={styles.email}
             name="email"
             id=""
-            placeholder="First Name"
-            type="text"
-            required
-          ></input>
-          <input
-            className={styles.name}
-            name="email"
-            id=""
-            placeholder="Last Name"
+            placeholder="you@example.com"
             type="email"
             required
           ></input>
+          <p className={styles.pPassword}>Password:</p>
+          <input
+            className={styles.password}
+            name="email"
+            id=""
+            placeholder="Password"
+            type="password"
+            required
+          ></input>
+          <button
+            type="submit"
+            className={styles.Submit}
+            onClick={() => {
+              handleHomeClick();
+              click();
+            }}
+          >
+            {text}
+          </button>
+          <p className={styles.Create}>
+            Already have an account?
+            <span>
+              <a onClick={handleLoginClick}> Login</a>
+            </span>
+          </p>
+          <div className={styles.lineGoogle}>
+            <div className={styles.lines}></div>
+            <p className={styles.signInGoogle}>or Signup with</p>
+            <div className={styles.lines}></div>
+          </div>
+          <button className={styles.google} onClick={googleClick}>
+            <FontAwesomeIcon
+              icon={faGoogle}
+              fontSize={25}
+              style={{ marginRight: "10px" }}
+            ></FontAwesomeIcon>
+            {GoogleText}
+          </button>
         </div>
-        <p>Email:</p>
-        <input
-          className={styles.email}
-          name="email"
-          id=""
-          placeholder="you@example.com"
-          type="email"
-          required
-        ></input>
-        <p className={styles.pPassword}>Password:</p>
-        <input
-          className={styles.password}
-          name="email"
-          id=""
-          placeholder="Password"
-          type="password"
-          required
-        ></input>
-        <button
-          type="submit"
-          className={styles.Submit}
-          onClick={() => {
-            handleHomeClick();
-            click();
-          }}
-        >
-          {text}
-        </button>
-        <p className={styles.Create}>
-          Already have an account?
-          <span>
-            <a onClick={handleLoginClick}> Login</a>
-          </span>
-        </p>
-        <div className={styles.lineGoogle}>
-          <div className={styles.lines}></div>
-          <p className={styles.signInGoogle}>or Signup with</p>
-          <div className={styles.lines}></div>
-        </div>
-        <button className={styles.google} onClick={googleClick}>
-          <FontAwesomeIcon
-            icon={faGoogle}
-            fontSize={25}
-            style={{ marginRight: "10px" }}
-          ></FontAwesomeIcon>
-          {GoogleText}
-        </button>
       </div>
-      <footer>© andinet international school</footer>
+      <div className={styles.foot}>© andinet international school</div>
     </>
   );
 }
